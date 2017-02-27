8463e39c7661f41673f134328f8de728e1472aaa
qinghe <qinghe@2dfire.com>
2017-02-27 17:21:38 +0800

    reports 入口文件

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -3,16 +3,15 @@ import React, {Component, PropTypes} from 'react';
 
 import SearchForm from "../../components/charts/SearchForm"
 import ChartWrapper from "../../components/charts/ChartWrapper"
-import DataTable from '../../components/datatable/DataTable'
-import DataForm from '../../components/dataform/DataForm'
+
 
 class Reports extends Component {
   render() {
     return (
       <div>
-        <DataForm />
+
         <ChartWrapper />
-        <DataTable />
+
       </div>
     )
   }
