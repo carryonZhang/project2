f28c4900f34c098b109116f62ab7a721a1181641
niuroubing <niuroubing@2dfire.com>
2017-03-06 11:01:59 +0800

    表单布局调整

--- src/components/dataform/index.js
+++ src/components/dataform/index.js
@@ -11,16 +11,13 @@ const formItemLayout = {
   wrapperCol: {span: 10}
 };
 const buttonItemLayout  = {
-  wrapperCol: {span: 14, offset: 1}
+  wrapperCol: { offset: 1}
 };
 const Option  = Select.Option;
 const MonthPicker = DatePicker.MonthPicker;
 class DataForm extends Component {
   constructor(props){
     super(props);
-  }
-  handleSelectChange(value) {
-    console.log(`selected ${value}`);
   }
 
   genOptions(options) {
@@ -34,137 +31,127 @@ class DataForm extends Component {
   }
 
   queryType(antd_form, querys) {
-    let form = [];
+    let controls = [];
     let _this = this;
     const { getFieldDecorator, getFieldsError, getFieldError } = antd_form;
     console.log('queryType中的查询条件', querys);
     querys.forEach(function(query) {
       // console.log('控件类型', query.fieldDataType);
       if(query.useLov === 'Y') {
-        form.push(
-            <FormItem
-                label={query.fieldShowName}
-                {...formItemLayout}
-            >
-              {
-                getFieldDecorator('shopName', {
-                  rules: [
-                    {required: false}
-                  ]
-                })
-                (
-                    <Select
-                        showSearch
-                        style={{ width: 200 }}
-                        optionFilterProp="children"
-                        placeholder={query.showDescription ? query.shortDescription : ''}
-                        initialValue={query.defaultValue}
-                        filterOption={(input, option) => option.props.value.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-                    >
-                      {
-                        _this.genOptions(query.lovEntity.values)
-                      }
-                    </Select> 
-                )
-              }
-             
-            </FormItem>
-        )
-      }else if(query.fieldDataType === 'DATE') {
-        form.push(
-            <FormItem label={query.fieldShowName}
+        controls.push(
+            <Col span={8}>
+              <FormItem
+                  label={query.fieldShowName}
                   {...formItemLayout}
               >
-              {
-
-                getFieldDecorator('date-picker', {
-                  rules: [
-                    {required: true, type: 'object', message: 'Please select time!'}
-                  ]
-                })(
-                    <DatePicker
-                        showTime
-                        placeholder="选择时间"
-                    />
-                )
-              }
+                {
+                  getFieldDecorator('shopName', {
+                    rules: [
+                      {required: false}
+                    ]
+                  })
+                  (
+                      <Select
+                          showSearch
+                          style={{ width: 200 }}
+                          optionFilterProp="children"
+                          placeholder={query.showDescription ? query.shortDescription : ''}
+                          initialValue={query.defaultValue}
+                          filterOption={(input, option) => option.props.value.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                      >
+                        {
+                          _this.genOptions(query.lovEntity.values)
+                        }
+                      </Select>
+                  )
+                }
 
               </FormItem>
+            </Col>
+
+        )
+      }else if(query.fieldDataType === 'DATE') {
+        controls.push(
+            <Col span={8}>
+              <FormItem label={query.fieldShowName}
+                  {...formItemLayout}
+              >
+                {
+
+                  getFieldDecorator('date-picker', {
+                    rules: [
+                      {required: true, type: 'object', message: 'Please select time!'}
+                    ]
+                  })(
+                      <DatePicker
+                          showTime
+                          placeholder="选择时间"
+                      />
+                  )
+                }
+
+              </FormItem>
+            </Col>
         )
       }else if(query.fieldDataType === 'MONTH') {
-        form.push(
-            <FormItem label={query.fieldShowName}
-                {...formItemLayout}
-            >
-              {
-                getFieldDecorator('month-picker', {
-                  rules: [
-                    {required: true, type: 'object', message: 'Please select time!'}
-                  ]
-                })(
-                    <MonthPicker />
-                )
-              }
+        controls.push(
+            <Col span={8}>
+              <FormItem label={query.fieldShowName}
+                  {...formItemLayout}
+              >
+                {
+                  getFieldDecorator('month-picker', {
+                    rules: [
+                      {required: true, type: 'object', message: 'Please select time!'}
+                    ]
+                  })(
+                      <MonthPicker />
+                  )
+                }
 
-            </FormItem>
+              </FormItem>
+            </Col>
         )
 
       }else if(query.fieldDataType === 'STRING') {
-        form.push(
-            <FormItem label={query.fieldShowName}
+        controls.push(
+            <Col span={8}>
+              <FormItem label={query.fieldShowName}
                   {...formItemLayout}
               >
-              {
-                getFieldDecorator('input', {
-                  rules: [
-                    {required: false}
-                  ]
-                })(
-                    <Input initialValue={query.defaultValue} placeholder={query.shortDescription} />
-                )
-              }
-
-            </FormItem>
+                {
+                  getFieldDecorator('input', {
+                    rules: [
+                      {required: false}
+                    ]
+                  })(
+                      <Input initialValue={query.defaultValue} placeholder={query.shortDescription} />
+                  )
+                }
+              </FormItem>
+            </Col>
         )
       }
     });
-    form.push(
-        <FormItem {...buttonItemLayout}>
-          <Button type="danger" htmlType="submit">查询</Button>
-          <span className={styles.separate}></span>
-          <Button type="danger">导出Excel</Button>
-        </FormItem>
+    controls.push(
+        <Col span={8} offset={16}>
+          <FormItem {...buttonItemLayout}>
+            <Button type="danger" htmlType="submit">查询</Button>
+            <span className={styles.separate}></span>
+            <Button type="danger">导出Excel</Button>
+          </FormItem>
+        </Col>
+
     );
     // console.log('queryType中生成的控件列表', form);
-    return form;
-  }
-
-  // colQuery(conditions){
-  //   for(let j = 0; j < 3; j++){
-  //     <Col span={8}>
-  //
-  //     </Col>
-  //   }
-  // }
-  
-  generateQuerys(antd_form){
-    let conditions = this.props.conditions;
-    // let rows = conditions.length / 3 + (conditions.length % 3 !== 0 ? 1 : 0);
-    // for(let i = 0; i < rows; i++){
-    //   return (
-    //       <Row>
-    //         {colQuery(conditions)}
-    //       </Row>
-    //   )
-    // }
-    // console.log('dataform组件中接收到的查询条件', this.props.conditions);
-    return this.queryType(antd_form, conditions);
+    return controls;
   }
 
   render(){
     const onSubmit = this.props.onSubmit;
     const reportId = this.props.reportId;
     const antd_form = this.props.form;
+    const conditions = this.props.conditions;
     return  (
         <Form className={styles.dataform} 
               onSubmit={(e) => {
@@ -179,7 +166,9 @@ class DataForm extends Component {
                 });
               }}
         >
-          {this.generateQuerys(antd_form)}
+          <Row>
+            {this.queryType(antd_form, conditions)}
+          </Row>
         </Form>
     );
   }
