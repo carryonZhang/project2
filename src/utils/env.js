e960af9aefff30f85ae9c9978d888e19853b108a
Kuncheng Zhao <csdk@outlook.com>
2017-03-22 22:45:42 +0800

    1

--- src/utils/env.js
+++ src/utils/env.js
@@ -5,7 +5,7 @@ const urlPrefix = {
     'DEV': 'http://10.1.7.61:8080/athena-api/', // 项目环境
     'DAILY': 'http://athena-api.2dfire-daily.com/',
     'PRE': 'http://athena-api.2dfire-pre.com/',
-    'PUBLISH': 'https://merchant-api.2dfire.com/',
+    'PUBLISH': 'https://athena-api.2dfire.com/',
 };
 
 // 获取当前运行时环境变量
