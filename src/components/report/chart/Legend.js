b05806053b281d9b39ce6dd7265907c0c44e6b36
qinghe <qinghe@2dfire.com>
2017-03-23 13:48:42 +0800

    加大首列宽度；调整颜色顺序；

--- src/components/report/chart/Legend.js
+++ src/components/report/chart/Legend.js
@@ -4,14 +4,39 @@ import styles from "./style.css"
 import {Row} from "antd"
 
 const Legend = ({legendSelected, onLegendChange}) => {
-
+        // 颜色的顺序要与grid中一致
         const colors = [
-            '#bb9f65',
-            '#ded28b',
             '#b83032',
-            '#fdf6de',
+            '#fa7921',
+            '#73d3dd',
+            '#bb9f65',
             '#024067',
-            '#4e4c67','#63001c','#f1f0e1','#ffd038','#383d3b','#5d737e','#fa7921','#73d3dd','#fe9920','#354649','#2f4554','453354','#393d54','#61a0a8','#c4ccd3','#c24b30','#ca8622','#ab4d49','#d6605c','#efbfbe','#749f83','#91c7ae','#d48265','#e0a590','#a59792'];
+            '#ded28b',
+            '#4e4c67',
+            '#fdf6de',
+            '#63001c',
+            '#ffd038',
+            '#383d3b',
+            '#f1f0e1',
+            '#ca8622',
+            '#91c7ae',
+            '#393d54',
+            '#ab4d49',
+            '#5d737e',
+            '#d48265',
+            '#749f83',
+            '#453354',
+            '#efbfbe',
+            '#354649',
+            '#fe9920',
+            '#a59792',
+            '#c24b30',
+            '#2f4554',
+            '#e0a590',
+            '#61a0a8',
+            '#d6605c',
+            '#c4ccd3',
+        ];
             // ['#c23531', '#2f4554', '#61a0a8', '#d48265', '#91c7ae', '#749f83', '#ca8622', '#bda29a', '#6e7074', '#546570', '#c4ccd3'];
         const labels = [];
         Object.keys(legendSelected).forEach((item, index) => {
